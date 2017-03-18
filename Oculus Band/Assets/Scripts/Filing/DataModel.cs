905867f69915355a03d38c27e5a0ed18bd181ebd
Robin Weiskopf <robin.weiskopf@gmail.com>
2017-03-18 11:30:40 +0100

    Adding Keyboard Integration and XML Serializer

---
+++ Oculus Band/Assets/Scripts/Filing/DataModel.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+using UnityEngine;
+
+namespace XMLDataModel
+{
+    [XmlRoot("Song")]
+    public class Song
+    {
+        private static string path = Application.persistentDataPath;
+
+        [XmlAttribute("name")]
+        public string name;
+
+        [XmlAttribute("duration")]
+        public float duration;
+
+        [XmlArray("notes")]
+        [XmlArrayItem("Note")]
+        public List<Note> notes = new List<Note>();
+
+
+
+        public Song(string name, List<Note> notes)
+        {
+            this.name = name;
+            this.notes = new List<Note>(notes);
+        }
+
+
+
+        public void Save()
+        {
+            string fpath = System.IO.Path.Combine(path, name + ".xml");
+            var serializer = new XmlSerializer(typeof(Song));
+            var stream = new FileStream(path, FileMode.Create);
+            serializer.Serialize(stream, this);
+
+            stream.Close();
+        }
+
+        public Song Load(string fpath)
+        {
+            var serializer = new XmlSerializer(typeof(Song));
+            var stream = new FileStream(path, FileMode.Open);
+            var container = serializer.Deserialize(stream) as Song;
+            stream.Close();
+
+            return container;
+        }
+    }
+
+    public class Note
+    {
+        public Note(float time, uint type) : this(time, type, false) { }
+
+        public Note(float time, uint type, bool bonus)
+        {
+            this.time = time;
+            this.type = type;
+            this.bonus = bonus;
+        }
+
+        [XmlAttribute("time")]
+        public float time;
+
+        [XmlAttribute("type")]
+        public uint type;
+
+        [XmlAttribute("bonus")]
+        public bool bonus;
+    }
+
+}
---
+++ Oculus Band/Assets/Scripts/Keyboard/KeyboardManager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardManager : MonoBehaviour {
+
+    private static LogitechGSDK.keyboardNames[][] zones = new LogitechGSDK.keyboardNames[5][] {
+        new LogitechGSDK.keyboardNames[] {
+            LogitechGSDK.keyboardNames.TILDE,
+            LogitechGSDK.keyboardNames.TAB,
+            LogitechGSDK.keyboardNames.CAPS_LOCK,
+            LogitechGSDK.keyboardNames.LEFT_SHIFT,
+            LogitechGSDK.keyboardNames.LEFT_CONTROL,
+            LogitechGSDK.keyboardNames.ONE,
+            LogitechGSDK.keyboardNames.Q,
+            LogitechGSDK.keyboardNames.A,
+            LogitechGSDK.keyboardNames.BACKSLASH,
+            LogitechGSDK.keyboardNames.LEFT_WINDOWS,
+            LogitechGSDK.keyboardNames.TWO,
+            LogitechGSDK.keyboardNames.W,
+            LogitechGSDK.keyboardNames.S,
+            LogitechGSDK.keyboardNames.Z
+        },
+        new LogitechGSDK.keyboardNames[] {
+            LogitechGSDK.keyboardNames.THREE,
+            LogitechGSDK.keyboardNames.E,
+            LogitechGSDK.keyboardNames.D,
+            LogitechGSDK.keyboardNames.X,
+            LogitechGSDK.keyboardNames.C,
+            LogitechGSDK.keyboardNames.LEFT_ALT,
+            LogitechGSDK.keyboardNames.FOUR,
+            LogitechGSDK.keyboardNames.R,
+            LogitechGSDK.keyboardNames.F,
+            LogitechGSDK.keyboardNames.V,
+            LogitechGSDK.keyboardNames.FIVE,
+            LogitechGSDK.keyboardNames.T,
+            LogitechGSDK.keyboardNames.G
+        },
+        new LogitechGSDK.keyboardNames[] {
+            LogitechGSDK.keyboardNames.SIX,
+            LogitechGSDK.keyboardNames.Y,
+            LogitechGSDK.keyboardNames.H,
+            LogitechGSDK.keyboardNames.B,
+            LogitechGSDK.keyboardNames.SEVEN,
+            LogitechGSDK.keyboardNames.U,
+            LogitechGSDK.keyboardNames.J,
+            LogitechGSDK.keyboardNames.N,
+            LogitechGSDK.keyboardNames.EIGHT,
+            LogitechGSDK.keyboardNames.I,
+            LogitechGSDK.keyboardNames.K,
+            LogitechGSDK.keyboardNames.M
+        },
+        new LogitechGSDK.keyboardNames[] {
+            LogitechGSDK.keyboardNames.NINE,
+            LogitechGSDK.keyboardNames.O,
+            LogitechGSDK.keyboardNames.L,
+            LogitechGSDK.keyboardNames.COMMA,
+            LogitechGSDK.keyboardNames.ZERO,
+            LogitechGSDK.keyboardNames.P,
+            LogitechGSDK.keyboardNames.SEMICOLON,
+            LogitechGSDK.keyboardNames.PERIOD,
+            LogitechGSDK.keyboardNames.RIGHT_ALT,
+            LogitechGSDK.keyboardNames.RIGHT_WINDOWS,
+            LogitechGSDK.keyboardNames.MINUS,
+            LogitechGSDK.keyboardNames.OPEN_BRACKET,
+            LogitechGSDK.keyboardNames.APOSTROPHE,
+            LogitechGSDK.keyboardNames.FORWARD_SLASH
+        },
+        new LogitechGSDK.keyboardNames[]
+        {
+            //LogitechGSDK.keyboardNames.CLOSE_BRACKET,
+            LogitechGSDK.keyboardNames.BACKSPACE,
+            LogitechGSDK.keyboardNames.ENTER,
+            LogitechGSDK.keyboardNames.RIGHT_SHIFT,
+            LogitechGSDK.keyboardNames.APPLICATION_SELECT,
+            LogitechGSDK.keyboardNames.RIGHT_CONTROL
+        }
+    };
+
+	// Use this for initialization
+	void Start () {
+        LogitechGSDK.LogiLedInit();
+
+		foreach(var key in zones[0])
+        {
+            LogitechGSDK.LogiLedSetLightingForKeyWithKeyName(key, 0, 100, 100);
+        }
+
+        foreach (var key in zones[1])
+        {
+            LogitechGSDK.LogiLedSetLightingForKeyWithKeyName(key, 0, 0, 100);
+        }
+
+        foreach (var key in zones[2])
+        {
+            LogitechGSDK.LogiLedSetLightingForKeyWithKeyName(key, 100, 0, 100);
+        }
+
+        foreach (var key in zones[3])
+        {
+            LogitechGSDK.LogiLedSetLightingForKeyWithKeyName(key, 0, 100, 0);
+        }
+
+        foreach (var key in zones[4])
+        {
+            LogitechGSDK.LogiLedSetLightingForKeyWithKeyName(key, 100, 100, 100);
+        }
+
+    }
+
+    private void OnDestroy()
+    {
+        LogitechGSDK.LogiLedShutdown();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
