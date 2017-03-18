a0fe6d00588baf3dc7b5bd3477ab9f23965b92ec
Simon Narduzzi <simon.narduzzi@epfl.ch>
2017-03-18 20:08:58 +0100

    Moved files

---
+++ Oculus Band/Assets/Scripts/Music/PlayerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+	public AudioManager audiomanager;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.V)) {
+			Debug.Log ("Failed voice");
+			audiomanager.Failed ("Voice");
+		}
+		if (Input.GetKeyDown (KeyCode.D)) {
+			Debug.Log ("Failed drums");
+			audiomanager.Failed ("Drums");
+		}
+		if (Input.GetKeyDown (KeyCode.L)) {
+			Debug.Log ("Failed piano");
+			audiomanager.Failed ("Piano");
+		}
+	}
+}
