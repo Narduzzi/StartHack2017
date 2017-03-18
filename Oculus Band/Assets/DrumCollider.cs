04d4a1e441ec4c21b2f4a334c858680587d64ada
Robin Weiskopf <robin.weiskopf@gmail.com>
2017-03-19 00:11:34 +0100

    Fixing GameManager

---
+++ Oculus Band/Assets/DrumCollider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumCollider : MonoBehaviour {
+
+    private Collider m_collider;
+    private Color backupColor;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_collider = gameObject.GetComponent<Collider>();
+        if(m_collider == null)
+        {
+            throw new MissingComponentException("No collider attached to drum");
+        }
+
+        backupColor = this.GetComponent<MeshRenderer>().material.color;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger");
+        if (other.gameObject.tag == "DrumStick")
+        {
+            this.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "DrumStick")
+        {
+            this.gameObject.GetComponent<MeshRenderer>().material.color = backupColor;
+        }
+    }
+}
--- Oculus Band/Assets/GameManager.cs
+++ Oculus Band/Assets/GameManager.cs
@@ -4,80 +4,100 @@ using UnityEngine;
 using XMLDataModel;
 
 public class GameManager : MonoBehaviour {
+    public MeshRenderer cubeRenderer;
+    public float toleranceTime = 0.2f;
+    public List<KeyCode> keys;
 
-	public string name;
-	private List<List<Note>> notesArray = new List<List<Note>>();
-	private float time;
-	public List<KeyCode> keys;
-	public float toleranceBeforeMs;
-	public float toleranceAfterMs; 
+    public string songName;
+    private List<Note> notes;
+    private List<List<Note>> notesArray = new List<List<Note>>();
+	private float startTime;
 	private bool valid = true;
-	private List<Note> notes;
+	
 	public string instrument;
 	public AudioManager audioManager;
 	private float treatmentTime = 0.3f;
+
+
+
 	// Use this for initialization
 	void Start () {
-		Song song = Song.Load (name);
+		Song song = Song.Load (songName);
 		notes = song.notes;
 		if (notes == null || notes.Count < 2) {
 			throw new MissingComponentException ("No song found");
 		}
+
 		notesArray = new List<List<Note>> ();
-		notesArray = StepReader.GenerateListByChannel(notesArray,keys,notes);
-		time = 0.0f;
+		notesArray = StepReader.GenerateListByChannel(notesArray, keys, notes);
+
+        startTime = Time.unscaledTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		time += Time.deltaTime;
-		notesArray = ReSyncList (notesArray, time);
+        float currentTime = Time.unscaledTime - startTime;
 
-		for (int i = 0; i < keys.Count; i++){
+        int i = 0;
+        foreach (var nl in notesArray)
+        {
+            if (nl.Count > 0)
+            {
+                Note currentNote = nl[0];
 
-			if (notesArray [i].Count > 0) {
-				float perfectTime = notesArray [i] [0].time;
-				Debug.Log (perfectTime + " " + time+treatmentTime+toleranceAfterMs+ " " + (perfectTime < time+treatmentTime+toleranceAfterMs));
-				if (perfectTime < time) {
-					
-					Debug.Log ("MISSED CHANNEL " + i);
-					Debug.LogError ("MISSED : PERFECT TIME WAS : " + perfectTime + " but now " + time);
-					valid = false;
-					TakeDecision (valid);
-				}
+                if (currentNote.time + toleranceTime < currentTime)
+                {
+                    valid = false;
+                    nl.Remove(currentNote);
+                }
+                else
+                {
+                    if (Input.GetKeyDown(keys[i]))
+                    {
+                        Debug.Log("Key pressed");
 
-				if (Input.GetKeyDown (keys [i])) {
-					if (time < perfectTime + toleranceAfterMs && time > perfectTime - toleranceBeforeMs) {
-						valid = true;
-					} else {
-						valid = false;
-					}
-					TakeDecision (valid);
-					break;
+                        if (Mathf.Abs(currentNote.time - currentTime) < toleranceTime)
+                        {
+                            valid = true;
+                        }
+                        else
+                        {
+                            valid = false;
+                        }
 
-				}
-			}
-		}
+                        nl.Remove(currentNote);
+                    }
+                }
+            }
+            i++;
+        }
+
+        UpdateBox();
+        UpdateAudioManager();
 	}
 
-	private void TakeDecision(bool valid){
-		if (!valid) {
-			audioManager.Failed (instrument);
-		} else {
-			audioManager.Success (instrument);
-			Debug.Log ("Success! " + instrument);
-		}
-	}
+    public void UpdateAudioManager()
+    {
+        if (valid)
+        {
+            audioManager.Success(instrument);
+        }
+        else
+        {
+            audioManager.Failed(instrument);
+        }
+    }
 
-	private List<List<Note>> ReSyncList(List<List<Note>> notesArray,float time){
-		for (int i = 0; i < notesArray.Count; i++) {
-			for (int j = 0; j < notesArray [i].Count; j++) {
-				Note note = notesArray [i] [j];
-				if (note.time < time) {
-					notesArray [i].Remove (note);
-				}
-			}
-		}
-		return notesArray;
-	}
+    private void UpdateBox()
+    {
+        if (valid)
+        {
+            cubeRenderer.material.color = Color.white;
+        }
+        else
+        {
+            cubeRenderer.material.color = Color.red;
+        }
+    }
+
 }
