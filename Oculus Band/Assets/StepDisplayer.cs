01cac1ab71031700830311384b2a31c25acec2ae
Simon Narduzzi <simon.narduzzi@epfl.ch>
2017-03-18 13:52:06 +0100

    Created falling steps

---
+++ Oculus Band/Assets/StepDisplayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XMLDataModel;
+
+public class StepDisplayer : MonoBehaviour {
+
+	//public List<Note> notes;
+	// Use this for initialization
+	public GameObject step;
+	private Vector3 initialPos;
+	private Quaternion initialRot;
+	void Start () {
+		initialPos = step.transform.position;
+		initialRot = step.transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			PushNote (0);
+		}
+	}
+
+	void PushNote(int offset){
+		GameObject newstep = GameObject.Instantiate (step,initialPos,initialRot);
+		newstep.transform.parent = this.transform;
+	}
+}
---
+++ Oculus Band/Assets/StepRecorder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XMLDataModel;
+
+public class StepRecorder : MonoBehaviour {
+
+	public AudioSource source;
+	public string name;
+	private Song song;
+	private List<Note> notes;
+	private float time;
+
+	public List<KeyCode> listKeys;	
+
+	// Use this for initialization
+	void Start () {
+		notes = new List<Note> ();
+		time = source.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < listKeys.Count; i++) {
+			if (Input.GetKeyDown (listKeys[i])) {
+				Note newNote = new Note (source.time, 1);
+				notes.Add (newNote);
+			}
+		}
+
+		if (source.time > source.clip.length/10.0) {
+			Debug.Log ("Finished!");
+			song = new Song (name, notes);
+			//song.Save ();
+		}
+	}
+}
