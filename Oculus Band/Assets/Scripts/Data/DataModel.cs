a03a537b589e723abf4623da31fbb69e10a1ad64
Simon Narduzzi <simon.narduzzi@epfl.ch>
2017-05-06 11:36:18 +0200

    Tracking OvrAvatarData

---
+++ Oculus Band/Assets/Scripts/Data/DataModel.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+using UnityEngine;
+
+namespace XMLDataModel {
+    [XmlRoot("Song")]
+    public class Song {
+        [XmlAttribute("name")]
+        public string name;
+
+        [XmlAttribute("duration")]
+        public float duration;
+
+        [XmlArray("notes")]
+        [XmlArrayItem("Note")]
+        public List<Note> notes = new List<Note>();
+
+        public Song() { }
+
+        public Song(string name, List<Note> notes) {
+            this.name = name;
+            this.notes = new List<Note>(notes);
+        }
+
+
+        private TextAsset GetAssetFromSongName(string songName) {
+            if (songName.EndsWith(".xml"))
+                throw new System.Exception("The asset should not have a file extension at the end!");
+
+            string fpath = Path.Combine("Tracks/", name);
+            return Resources.Load<TextAsset>(fpath);
+        }
+
+        public Song LoadSong(string songName) {
+            var fpath = GetAssetFromSongName(name);
+            return Load(fpath);
+        }
+
+
+        public void SaveSong() {
+            var fpath = GetAssetFromSongName(name);
+            Save(fpath);
+        }
+
+        public static Song Load(string fpath) {
+            TextAsset songAsset = Resources.Load(fpath) as TextAsset;
+            return Load(songAsset);
+        }
+
+        public static Song Load(TextAsset songAsset) {
+            if (songAsset == null)
+                throw new System.Exception("Song does not exist!");
+
+            var serializer = new XmlSerializer(typeof(Song));
+            using (Stream ms = new MemoryStream(songAsset.bytes)) {
+                var stream = new StreamReader(ms, System.Text.Encoding.UTF8, true);
+                return serializer.Deserialize(stream) as Song;
+            }
+        }
+
+        public void Save() {
+            TextAsset songAsset = GetAssetFromSongName(name);
+            Save(songAsset);
+        }
+
+        public void Save(TextAsset songAsset) {
+            if (songAsset == null)
+                throw new System.Exception("Song does not exist!");
+
+            var serializer = new XmlSerializer(typeof(Song));
+            using (Stream ms = new MemoryStream(songAsset.bytes)) {
+                var stream = new StreamWriter(ms, System.Text.Encoding.UTF8);
+                serializer.Serialize(stream, this);
+            }
+        }
+
+    }
+
+    public class Note {
+        public Note() { }
+
+        public Note(float time, uint type) : this(time, type, false) { }
+
+        public Note(float time, uint type, bool bonus) {
+            this.time = time;
+            this.type = type;
+            this.bonus = bonus;
+        }
+
+        [XmlAttribute("time")]
+        public float time;
+
+        [XmlAttribute("type")]
+        public uint type;
+
+        [XmlAttribute("bonus")]
+        public bool bonus;
+    }
+
+}
---
+++ Oculus Band/Assets/Scripts/Guitar/GuitarTracker.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class GuitarTracker : MonoBehaviour {
+
+    [SerializeField]
+    private Transform anchor;
+
+    [SerializeField]
+    private Vector3 anchorOffset = new Vector3(0.0f, 0.0f, 0.0f);
+
+    [SerializeField]
+    private Transform directionalAnchor;
+
+    private Quaternion initialRotation;
+    private Vector3 initialPosition;
+
+    // Use this for initialization
+    void Start() {
+        CheckComponents();
+
+        initialRotation = this.transform.localRotation;
+        initialPosition = this.transform.localPosition;
+    }
+
+    private void CheckComponents() {
+        if (anchor == null) throw new MissingReferenceException("No tracking hand defined");
+        if (directionalAnchor == null) throw new MissingReferenceException("No direction hand defined");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        Vector3 direction = (directionalAnchor.position - anchor.position - anchorOffset).normalized;
+        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.forward);
+
+        this.transform.rotation = rotation * initialRotation;
+        this.transform.position = initialPosition + anchor.position + anchorOffset;
+
+    }
+}
