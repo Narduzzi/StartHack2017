e44751fce556a7435d620b3a8deea2faed2ee6a9
Simon Narduzzi <simon.narduzzi@epfl.ch>
2017-05-16 21:11:55 +0200

    Added first example of piano - failed

---
+++ Oculus Band/Assets/KeyStabilizer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyStabilizer : MonoBehaviour {
+	public float MinRotX = -3.5f;
+	public float MaxRotX = 0.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		checkRotation ();
+	}
+
+	public void checkRotation(){
+		Quaternion rotation = this.transform.rotation;
+		Vector3 rot3 = rotation.eulerAngles;
+		if (360 - rot3.x >= MaxRotX) {
+			rot3.x = MaxRotX;
+		}
+		if (360 - rot3.x <= MinRotX) {
+			rot3.x = MinRotX;
+		}
+	}
+}
---
+++ Oculus Band/Assets/PositionStabilizer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionStabilizer : MonoBehaviour {
+	Vector3 initialPosition;
+	Quaternion initialRotation;
+	Rigidbody rigidbody;
+
+	public Vector3 ForceVector;
+	// Use this for initialization
+	void Start () {
+		this.initialPosition = this.transform.position;
+		this.initialRotation = this.transform.rotation;
+		rigidbody = this.transform.GetComponent<Rigidbody> ();
+		if (rigidbody == null) {
+			Debug.LogError ("No rigidbody attached to : " + this.transform.name);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 actualPosition = this.transform.position;
+		//specific to key
+		if (actualPosition.y > initialPosition.y) {
+			this.transform.position = initialPosition;
+		}
+
+		//add return force
+		Vector3 forceToApply = computeForce ();
+		rigidbody.AddForceAtPosition (forceToApply, this.transform.position);
+	
+
+	}
+
+	private Vector3 computeForce(){
+		Vector3 actualPosition = this.transform.position;
+		Vector3 substracted = initialPosition - actualPosition;
+		Vector3 force = new Vector3 (substracted.x * ForceVector.x, substracted.y * ForceVector.y, substracted.z * ForceVector.z);
+		return force;
+	}
+}
