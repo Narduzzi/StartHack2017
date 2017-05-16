f10b97a66ae26b91a81177d22f6a3ed799229d49
Simon Narduzzi <simon.narduzzi@epfl.ch>
2017-05-16 22:11:11 +0200

    fixed bug with collider (key remains down)

---
+++ Oculus Band/Assets/MarkerGiver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerGiver : MonoBehaviour {
+
+	List<GameObject> listBones3 = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+		listBones3 = updateListBone3 ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	List<GameObject> updateListBone3(){
+		return null;
+	}
+}
