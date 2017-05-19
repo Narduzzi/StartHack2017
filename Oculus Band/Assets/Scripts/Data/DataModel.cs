42843c8c9e04b67721b2f51c9c37aa044e5c3d3a
Simon Narduzzi <simon.narduzzi@epfl.ch>
2017-05-19 22:01:33 +0200

    Found bug in DataModel (Method "Save()")

--- Oculus Band/Assets/Scripts/Data/DataModel.cs
+++ Oculus Band/Assets/Scripts/Data/DataModel.cs
@@ -5,6 +5,7 @@ using System.Xml;
 using System.Xml.Serialization;
 
 using UnityEngine;
+using System;
 
 namespace XMLDataModel {
     [XmlRoot("Song")]
@@ -64,6 +65,9 @@ namespace XMLDataModel {
 
         public void Save() {
             TextAsset songAsset = GetAssetFromSongName(name);
+			if (songAsset == null) {
+				Debug.LogError ("The asset \"" + name + "\" does not exist");
+			}
             Save(songAsset);
         }
 
@@ -72,10 +76,14 @@ namespace XMLDataModel {
                 throw new System.Exception("Song does not exist!");
 
             var serializer = new XmlSerializer(typeof(Song));
-            using (Stream ms = new MemoryStream(songAsset.bytes)) {
-                var stream = new StreamWriter(ms, System.Text.Encoding.UTF8);
-                serializer.Serialize(stream, this);
-            }
+			try{
+	            using (Stream ms = new MemoryStream(songAsset.bytes)) {
+	                var stream = new StreamWriter(ms, System.Text.Encoding.UTF8);
+	                serializer.Serialize(stream, this);
+	            }
+			}catch(Exception e){
+				Debug.LogError ("Could not save song. Error in MemoryStream");
+			}
         }
 
     }
