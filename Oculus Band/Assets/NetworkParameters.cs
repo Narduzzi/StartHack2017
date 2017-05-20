e33d90a5894e12ca858518e49709980e9beb184a
Simon Narduzzi <simon.narduzzi@epfl.ch>
2017-05-20 16:48:38 +0200

    Player spawn behind his instrument (Selected at Connection scene)

---
+++ Oculus Band/Assets/NetworkParameters.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkParameters : MonoBehaviour {
+
+	public enum Instrument{Guitar,Piano,Drums};
+	public enum HeadSet{Desktop,Oculus};
+	public enum Hands{LeapMotion,Touch};
+
+	public Hands hands = Hands.LeapMotion;
+	public HeadSet headSet = HeadSet.Desktop;
+	public Instrument instrument = Instrument.Piano;
+
+	void Awake(){
+		DontDestroyOnLoad (this.gameObject);
+	}
+
+	public string getInstrument(){
+		return instrument.ToString ();
+	}
+	public string getHeadset(){
+		return headSet.ToString ();
+	}
+	public string getHands(){
+		return hands.ToString ();
+	}
+
+	public void setInstrument(Instrument instrument){
+		this.instrument = instrument;
+	}
+
+	public void setHands(Hands hands){
+		this.hands = hands;
+	}
+
+	public void setHeadset(HeadSet headSet){
+		this.headSet = headSet;
+	}
+
+}
---
+++ Oculus Band/Assets/SetupConcertScene.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupConcertScene : MonoBehaviour {
+
+	public Transform PianoPlayerTransform;
+	public Transform DrumsPlayerTransform;
+	public Transform GuitarPlayerTransform;
+	public GameObject CameraPlayer;
+
+	private NetworkParameters parameters;
+	private string instrument;
+	private string headset;
+	private string hands;
+
+	// Use this for initialization
+	void Awake () {
+		GameObject ParamsGO = GameObject.Find ("Params");
+		if (ParamsGO == null) {
+			Debug.LogError ("No \"Params\" GameObject found in scene");
+		}
+
+		parameters = ParamsGO.GetComponent<NetworkParameters> ();
+		if (parameters == null) {
+			Debug.LogError ("No script \"NetworkParameters\" attached to GameObject \"Params\"");
+		}
+
+		//Checking transforms
+		if (PianoPlayerTransform == null) {
+			Debug.LogError ("PianoPlayerTransform is null");
+		}
+		if (GuitarPlayerTransform == null) {
+			Debug.LogError ("GuitarPlayerTransform is null");
+		}
+		if (DrumsPlayerTransform == null) {
+			Debug.LogError ("DrumsPlayerTransform is null");
+		}
+		if (CameraPlayer == null) {
+			Debug.LogError ("CameraPlayer is null");
+		}
+
+		instrument = parameters.getInstrument ();
+		headset = parameters.getHeadset ();
+		hands = parameters.getHands ();
+		Setup (instrument, headset, hands);
+	}
+
+
+
+	void Setup(string instrument, string headset, string hands){
+		if (instrument == "Piano") {
+			SetupPlayerLocation (PianoPlayerTransform);
+		} else if (instrument == "Guitar") {
+			SetupPlayerLocation (GuitarPlayerTransform);
+		} else if (instrument == "Drums") {
+			SetupPlayerLocation (DrumsPlayerTransform);
+		} else {
+			Debug.LogError ("Instrument \"" + instrument + "\" was not setup in game. No such instrument exists.");
+		}
+
+
+		//Parameters for headset and hands
+	}
+		
+	void SetupPlayerLocation(Transform transform){
+		CameraPlayer.transform.position = transform.position;
+		CameraPlayer.transform.rotation = transform.rotation;
+		CameraPlayer.transform.parent = transform.parent;
+	}
+		
+	void SetupHeadSet(){
+		Debug.LogWarning ("SetupHeadSet method is not implemented");
+	}
+
+	void SetupHands(){
+		Debug.LogWarning ("SetupHands method is not implemented");
+	}
+}
