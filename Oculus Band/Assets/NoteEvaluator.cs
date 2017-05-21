244b0c61e7d8963aaeaad001465f0aafb63c6925
Simon Narduzzi <simon.narduzzi@epfl.ch>
2017-05-21 17:13:17 +0200

    Glowing animations

--- Oculus Band/Assets/NoteEvaluator.cs
+++ Oculus Band/Assets/NoteEvaluator.cs
@@ -5,9 +5,33 @@ using UnityEngine;
 public class NoteEvaluator : MonoBehaviour {
 
 	public bool entered = false;
-	public bool played = false;
 	public bool failed = false;
+	public GameObject Rails;
+	public Color normalColor;
+	public Color errorColor;
 	private GameObject colliding;
+	private GameObject RailL;
+	private GameObject RailR;
+	private Material RailMaterialL;
+	private Material RailMaterialR;
+
+	void Start(){
+		if (Rails == null) {
+			Debug.LogError ("No Rails object passed");
+		} 
+		if (normalColor == null) {
+			Debug.LogError ("No normalColor found");
+		}
+		if (errorColor == null) {
+			Debug.LogError ("No errorColor found");
+		}
+
+		RailL = Rails.transform.FindChild ("RailLeft").gameObject;
+		RailR = Rails.transform.FindChild ("RailRight").gameObject;
+		RailMaterialL = RailL.GetComponent<Renderer> ().material;
+		RailMaterialR = RailR.GetComponent<Renderer> ().material;
+		RestoreRailColor ();
+	}
 
 	void OnTriggerEnter(Collider collider){
 		Debug.Log ("Trigger Entered");
@@ -18,7 +42,7 @@ public class NoteEvaluator : MonoBehaviour {
 	void OnTriggerExit(Collider collider){
 		//Missed GameObject
 		if (colliding != null) {
-			colliding.GetComponent<Renderer> ().material.color = Color.red;
+			FailObject ();
 		}
 
 		entered = false;
@@ -27,19 +51,37 @@ public class NoteEvaluator : MonoBehaviour {
 	}
 
 	public void RestoreState(){
+		RestoreRailColor ();
 		entered = false;
 		failed = false;
 	}
-	public void PlayNote(){
-		played = true;
+
+	private void RestoreRailColor(){
+		StartCoroutine(FadeColor(normalColor,errorColor,1.0f));
+	}
+
+	private IEnumerator FadeColor(Color start, Color end, float time){
+		float elapsedTime = 0;
+		RailMaterialL.color = start;
+		RailMaterialR.color = start;
+        while (elapsedTime < time){
+			RailMaterialL.color = Color.Lerp(start, end, (elapsedTime / time));
+			RailMaterialR.color = Color.Lerp(start, end, (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
 	}
 
 	public void AnimateCollidingObject(){
 		//change to green 
 		//deactivate collider
 		//(and animate destroy)
-		colliding.GetComponent<Renderer> ().material.color = Color.cyan;
+		colliding.GetComponent<StepFaller> ().AnimateWin();
 		colliding.GetComponent<Collider> ().enabled = false;
 
+	}
+
+	public void FailObject(){
+		colliding.GetComponent<StepFaller> ().AnimateFail();
 	}
 }
--- Oculus Band/Assets/Resources/Prefabs/ChannelsUI/StepFaller.cs
+++ Oculus Band/Assets/Resources/Prefabs/ChannelsUI/StepFaller.cs
@@ -7,16 +7,29 @@ public class StepFaller : MonoBehaviour {
 	private float createdTime = 0f;
 	public float MaxTime = 20.0f;
 	public bool original = true;
+	public Color winColor = Color.green;
+	public Color failColor = Color.red;
+	private GameObject around;
+	private bool finished = false;
 	// Use this for initialization
 	void Start () {
 		createdTime = 0.0f;
+		around = this.transform.FindChild ("Around").gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		float newY = this.transform.localPosition.y - speed * Time.deltaTime;
+		float newZ = this.transform.localPosition.z;
+
+		if (finished) {
+			newZ = this.transform.localPosition.z - speed * Time.deltaTime;
+		}
+
 		Vector3 pos = this.transform.localPosition;
 		pos.y = newY;
+		pos.z = newZ;
 		this.transform.localPosition = pos;
 		createdTime += Time.deltaTime;
 		if (createdTime > MaxTime) {
@@ -36,4 +49,21 @@ public class StepFaller : MonoBehaviour {
 		this.transform.position = spawn_pos;
 		//this.transform.parent = parent;
 	}
+
+	public void AnimateWin(){
+		around.GetComponent<Renderer> ().material.color = winColor;
+		StartCoroutine (ElevateDestroy ());
+	}
+
+	public void AnimateFail(){
+		around.GetComponent<Renderer> ().material.color = failColor;
+	}
+
+	private IEnumerator ElevateDestroy(){
+		finished = true;
+		yield return new WaitForSeconds (0.8f);
+		GameObject.Destroy(this.gameObject);
+	}
+
+
 }
